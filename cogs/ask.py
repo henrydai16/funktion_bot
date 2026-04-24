9a40949b7b6921d05f545c757491e434e5b5d08d
henrydai <henrydai@umich.edu>
2026-04-23 21:45:29 -0400

    added random replies

--- cogs/ask.py
+++ cogs/ask.py
@@ -4,11 +4,16 @@ import ollama
 import os
 from discord.ext import commands
 from utils.data import save_values
-
+import textwrap
 
 class Ask(commands.Cog):
     def __init__(self, bot): # Calls when loading cog
         self.bot = bot
+
+    def chunk_text(text, limit=1900):
+        for i in range(0, len(text), limit):
+            yield text[i:i + limit]
+
     @commands.command(
         brief="Ask FunKtion history via: !ask '[prompt]'",
         help=(
@@ -19,7 +24,7 @@ class Ask(commands.Cog):
     async def ask(self, ctx):
         """Uses an LLM/RAG model connected to FunKtion information via ollama."""
         # Ollama Configuration
-        model = "phi3:mini"
+        model = "qwen3:14b"
         # system_prompt = """
         #                 you are an assistant that likes pizza.
         #                 """
@@ -31,18 +36,20 @@ class Ask(commands.Cog):
                         that people here are dancers. Tend to keep responses below 
                         1000 characters.
                         """
-
+        # Have the prompt be after !ask
+        prompt = ctx.message.content[len(ctx.prefix + "ask "):]
         response = ollama.chat(
             model=model,
             messages=[
                 {"role": "system", "content": system_prompt},
-                {"role": "user", "content": ctx.message.content},
+                {"role": "user", "content": prompt},
             ],
         )
-        content = response.message.content
+
         # Discord sends messages in 2000 chr limits, splits up messages.
-        for i in range(0, len(content), 2000):
-            await ctx.send(f"{ctx.author.mention} {content[i:i+2000]}")
+        content = response.message.content
+        for chunk in chunk_text(content):
+            await ctx.send(f"{ctx.author.mention} {chunk}")
 
 async def setup(bot):
     await bot.add_cog(Ask(bot))
--- cogs/events.py
+++ cogs/events.py
@@ -2,10 +2,24 @@ import discord
 from discord.ext import commands
 import random
 import re
+import time
+
+# Will be used to randomly insult a person.
+ROASTS = [
+    "You little shit.",
+    "W message",
+    "L message",
+    "shore shore!",
+    "HOLY! Just kiss a dude at that point man.",
+    "Holy glaze."
+]
 
 class Events(commands.Cog):
+    # ONLY send troll random replies every (minimum) 30 minutes. (only for replies NOT triggers)
+    COOLDOWN = 60 * 30  
     def __init__(self, bot):
         self.bot = bot
+        self.last_roast_time = {}  # (channel_id, keyword) -> timestamp
 
     # @ = decorator, MUST be called on_ready(), follow intents documentation
     @commands.Cog.listener()
@@ -25,31 +39,47 @@ class Events(commands.Cog):
         triggers = {
             "ass":    "By 'ass', did you mean 'Kevin's ass'?",
             "galaxy": "By 'galaxy', did you mean 'Kevin's ass'?",
+            "gargantuan": "By 'gargantuan', did you mean 'Kevin's ass'?",
             "best":   "By 'best', did you mean 'best dancer Yaani?'",
             "gay":    "By 'gay', did you mean 'Justin kissing"
         }
 
         msg = message.content.lower()
 
-        # Loop through triggers
+        #1 TRIGGER WORDS lead to a response. 
         for keyword, response in triggers.items():
             # Match whole word + optional punctuation
             pattern = rf"\b{re.escape(keyword)}\b[^\w]?"
 
+            key = (message.channel.id, keyword)
             if re.search(pattern, msg):
                 # Special case: message is exactly "gay"
                 if keyword == "gay":
                     names = ["Anthony", "Lucas", "Adli", "Sean", "Yaani", "Brian", "Tyler"]
                     chosen = random.choice(names)
                     response = f"{response} {chosen}?'"
-
                 await message.channel.send(f"{message.author.mention} {response}")
-                break
-        # Because we've overwritten default behavior w/ custom events (above),
-        # the bot will no longer automatically process_commands.
-        # After custom code, NOW call process_commands at the end
-        # await self.bot.process_commands(message) -- caused clashing of cog v. bot processing
-        return
+
+                # Because we've overwritten default behavior w/ custom events (above),
+                # the bot will no longer automatically process_commands.
+                # After custom code, NOW call process_commands at the end
+                # await self.bot.process_commands(message) -- caused clashing of cog v. bot processing
+                return
+        
+        #2 RANDOM ROAST if no trigger words are detected.
+        if random.random() < 0.99: # 1% chance to randomly reply to ANY message
+            now = time.time()
+            key = message.channel.id
+            last = self.last_roast_time.get(key, 0)
+
+            if now - last > self.COOLDOWN:
+                self.last_roast_time[key] = now
+
+                roast = random.choice(ROASTS)
+                await message.reply(f"{message.author.mention} {roast}")
+                return
+                
+
 
 async def setup(bot):
     await bot.add_cog(Events(bot))
