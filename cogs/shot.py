e9e68033c70fb5344bc5f71d75452a9b9dea5ba5
henrydai <henrydai@umich.edu>
2026-06-26 13:57:27 -0400

    usage of enums and starting rules on giving shots

--- cogs/shot.py
+++ cogs/shot.py
@@ -2,6 +2,15 @@ import discord
 import asyncio
 from discord.ext import commands
 from utils.data import save_values
+from enum import IntEnum
+
+# ALUM and OLDIE are both 3 since they have the same amount of power
+# from the bot's perspective. Used in determining "handing out shots"
+class RolePower(IntEnum):
+    ALUM = 3
+    OLDIE = 3
+    TWEENIE = 2
+    NEWBIE = 1
 
 # https://guide.pycord.dev/popular-topics/cogs
 class Shot(commands.Cog):
@@ -28,7 +37,7 @@ class Shot(commands.Cog):
         ),
         signature =""
     )
-    async def shot(self, ctx, member: discord.Member = None, amount: int = None):
+    async def shot(self, ctx, member: discord.Member = None, amount: int = None) -> None:
         """
         Assigns a number of 'shots' to a user and stores the total in persistent JSON.
         """
@@ -53,15 +62,9 @@ class Shot(commands.Cog):
         else:
             print(f"shot should not go through here: {str(ctx_author_hi_role)} < {str(member_hi_role)} ")
 
-
-
-        # ctx_author_roles = ctx.author.roles
-        # print(f"this is the authors role: {ctx_author_roles}")
-        # print(f"this is the members role: {member.roles}")
-
-        values = self.bot.user_values
-        uid = str(member.id)
-
+        # Only allow default shot behavior if author is oldie or alum
+        if str(ctx_author_hi_role) == "oldie" or str(ctx_author_hi_role) == "alum":
+            print("i am an oldie")  
 
         # Specified shot amount not present, default to 1 shot  
         if amount is None:
@@ -73,6 +76,9 @@ class Shot(commands.Cog):
         
         # Punish user for receiving > 0 shots.
         else:
+            values = self.bot.user_values
+            uid = str(member.id)
+
             # Update json score if user_values is missing
             new_shot_count = values.get(uid, 0) + amount
             values[uid] = new_shot_count
@@ -81,7 +87,7 @@ class Shot(commands.Cog):
             await ctx.send(f"{member.mention} now has **{values[uid]}** shots. Be better.")
 
 
-    async def reward(self, ctx, member: discord.Member = None, amount: int = 1):
+    async def reward(self, ctx, member: discord.Member = None, amount: int = 1) -> None:
         """
         Removes (rewards) a number of 'shots' from a user and updates json.
         Originally it's own command, now integrated with !shot if the value is < 0.
@@ -108,10 +114,10 @@ class Shot(commands.Cog):
         Alumni → Oldie → Tweenie → Newbie
         """
         priority_ids = [
-            953499388351750144, # Alumni
-            953499007408308224, # Oldie
-            953498912411500574, # Tweenie
-            953498765694730281, # Newbie
+            953499388351750144, # alum
+            953499007408308224, # oldie
+            953498912411500574, # tweenie
+            953498765694730281, # newbie
         ]
 
         for pid in priority_ids:
@@ -120,6 +126,53 @@ class Shot(commands.Cog):
                     return r
         # If no ID found, default to Newbie status.
         return next((role for role in member.guild.roles if role.id == priority_ids[-1]), None)
+    
+    def can_give_shot(self, author_role: discord.Role, member_role: discord.Role,
+                       amount: int) -> tuple[bool, int]:
+        """
+        Given an author role and a member role, returns a tuple on whether or not
+        they will be able to give a shot. 
+        Oldies/Alumnis can give a shot to anyone
+        Tweenies will be offered a one time use ON NEWBIES.
+        Newbies will be deceived into thinking they can hand out shots.
+        """
+
+        PRIORITY = {
+            "alum": RolePower.ALUM,
+            "oldie": RolePower.OLDIE,
+            "tweenie": RolePower.TWEENIE,
+            "newbie": RolePower.NEWBIE
+        }
+
+        author_power = PRIORITY.get(author_role.name, RolePower.NEWBIE)
+        member_power = PRIORITY.get(member_role.name, RolePower.NEWBIE)
+
+        # oldie / alum are able to hand out shots
+        if author_power >= RolePower.OLDIE:
+            return tuple(True, amount)
+
+        # If tweenie detected, see if member is oldie or newbie:
+        elif author_power == RolePower.TWEENIE:
+
+            # Trying to give a shot to oldie is not allowed.
+            if member_power == RolePower.OLDIE:
+                return False
+            
+            # Use "shot pass" on another tweenie/newbie (refreshes every 7d?) 
+            else: 
+
+                # check_shot_pass()
+                return True
+
+        # newbies will be deceived into using the one time pass
+        else: # author_power == RolePower.NEWBIE
+
+
+    def tweenie_give_shot(self, author_role: discord.Role):
+        """
+        Verifies if tweenie is able to give a shot. If a shot pass is present,
+        they will be prompted if they want to use it.
+        """
 
 
 async def setup(bot):
